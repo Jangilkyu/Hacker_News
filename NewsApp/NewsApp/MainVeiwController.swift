ce8ddda12323cd34aa3e3b5691ae5f52402aab22
장일규 <>
2022-01-11 21:15:38 +0900

    [ADD] BASE CODE

---
+++ NewsApp/NewsApp/MainVeiwController.swift
@@ -0,0 +1,8 @@
+//
+//  MainVeiwController.swift
+//  NewsApp
+//
+//  Created by 장일규 on 2022/01/10.
+//
+
+import Foundation
---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by 장일규 on 2022/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
