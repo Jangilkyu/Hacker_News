ebb7ba390cf2b0d062337f69697acd158519a546
장일규 <jang19.dev@gmail.com>
2022-01-29 23:50:02 +0900

    Feature getStory

---
+++ NewsApp/NewsApp/Controllers/MainNavigationController.swift
@@ -0,0 +1,8 @@
+//
+//  MainNavigationController.swift
+//  NewsApp
+//
+//  Created by 장일규 on 2022/01/29.
+//
+
+import Foundation
---
+++ NewsApp/NewsApp/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  NewsApp
+//
+//  Created by 장일규 on 2022/01/29.
+//
+
+import Foundation
